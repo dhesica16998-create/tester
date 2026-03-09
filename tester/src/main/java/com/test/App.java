ef6cd3e1ed08b8c9073e1ea2edc4d1e5a05e4792
Dhesica V <dhesica16998@gmail.com>
2026-03-09 11:32:17 +0530

    first commit

---
+++ tester/src/main/java/com/test/App.java
@@ -0,0 +1,18 @@
+package com.test;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+        
+        int length = input.length(); 
+        
+        System.out.println("Number of characters in the string: " + length);
+        
+        sc.close();
+    }
+}
